6304a350f29ee8acf8436e12c21f72e5425ef9b5
lorenicontecha <angielorenicontecha9815@gmail.com>
2021-02-27 16:03:52 -0500

    :tada: Test-Ceiba | creación de la estructura del proyecto

---
+++ src/main/java/com/prueba/ceiba/PruebaCeibaApplication.java
@@ -0,0 +1,13 @@
+package com.prueba.ceiba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCeibaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCeibaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/prueba/ceiba/PruebaCeibaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prueba.ceiba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCeibaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
